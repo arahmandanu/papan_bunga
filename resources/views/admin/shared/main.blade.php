2d5fba788383426ccb05bc3d4925ef33bffd8398
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2024-07-17 00:52:07 +0700

    asdasd

--- resources/views/admin/shared/main.blade.php
+++ resources/views/admin/shared/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <title>Dashboard - Admin Papan Bunga</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
